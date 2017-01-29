e1885b5ec8c242af94cb46ea72a1dac5ffcf9503
IhorZhuk <ihor.zhuk92@gmail.com>
2017-01-29 15:01:11 +0100

    Initial commit

---
+++ user-settings.js
@@ -0,0 +1,15 @@
+// Place your settings in this file to overwrite the default settings
+{
+    "editor.fontSize": 15,
+    "editor.tabSize": 2,
+    "editor.parameterHints": false,
+    "editor.quickSuggestions": false,
+    "files.associations": {
+      "*.eco": "html"
+    }
+  ,
+  "editor.renderIndentGuides": true,
+  "editor.codeLens": false,
+  "editor.renderLineHighlight": "none",
+  "explorer.openEditors.visible": 0,
+}
