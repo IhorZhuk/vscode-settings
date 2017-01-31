bfa79b80d524d4a071e0993cc5474ba3c0edfe58
IhorZhuk <ihor.zhuk92@gmail.com>
2017-01-31 14:53:09 +0100

    Update settings

--- user-settings.js
+++ user-settings.js
@@ -1,13 +1,16 @@
 // Place your settings in this file to overwrite the default settings
 {
-    "editor.fontSize": 15,
-    "editor.tabSize": 2,
-    "editor.parameterHints": false,
-    "editor.quickSuggestions": false,
+   "editor.fontSize": 15,
+   "editor.tabSize": 2,
+    
     "files.associations": {
-      "*.eco": "html"
-    }
-  ,
+        "*.hamlc" : "haml",
+        "*.jst.eco" : "html",
+        "*.tpl" : "html",
+    },
+  
+  "editor.parameterHints": false,
+  "editor.quickSuggestions": false,
   "editor.renderIndentGuides": true,
   "editor.codeLens": false,
   "editor.renderLineHighlight": "none",
