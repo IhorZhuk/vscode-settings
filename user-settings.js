2199eee869d78d18047376d5a88ac52fdf0b59c4
IhorZhuk <ihor.zhuk92@gmail.com>
2017-06-12 09:34:43 +0200

    Update user-settings.js

--- user-settings.js
+++ user-settings.js
@@ -1,18 +1,24 @@
 // Place your settings in this file to overwrite the default settings
 {
-   "editor.fontSize": 15,
-   "editor.tabSize": 2,
-    
+    // Controls the font size in pixels.
+    "editor.fontSize": 14,
+    "editor.tabSize": 2,
+
+     // Configure file associations to languages (e.g. "*.extension": "html"). These have precedence over the default associations of the languages installed.
     "files.associations": {
         "*.hamlc" : "haml",
         "*.jst.eco" : "html",
         "*.tpl" : "html",
     },
-  
-  "editor.parameterHints": false,
-  "editor.quickSuggestions": false,
-  "editor.renderIndentGuides": true,
-  "editor.codeLens": false,
-  "editor.renderLineHighlight": "none",
-  "explorer.openEditors.visible": 0,
+
+    "editor.parameterHints": false,
+    "editor.quickSuggestions": false,
+    "editor.renderIndentGuides": true,
+    "editor.codeLens": false,
+    "editor.renderLineHighlight": "none",
+    "explorer.openEditors.visible": 0,
+    "workbench.colorTheme": "Default Light+",
+    "workbench.iconTheme": "vs-minimal",
+    "workbench.welcome.enabled": false,
+    "editor.minimap.enabled": false
 }
