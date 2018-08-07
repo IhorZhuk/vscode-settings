52e14b970a1d866e817411b38cf79a64c16638b8
Ihor Zhuk <ihor.zhuk92@gmail.com>
2018-08-07 11:49:08 +0200

    enable code fodling in jsx files

--- user-settings.js
+++ user-settings.js
@@ -1,17 +1,21 @@
 // Place your settings in this file to overwrite the default settings
 {
     // Controls the font size in pixels.
-    "editor.fontSize": 15,
+    "editor.fontSize": 16,
     "editor.tabSize": 2,
 
      // Configure file associations to languages (e.g. "*.extension": "html"). These have precedence over the default associations of the languages installed.
     "files.associations": {
         "*.hamlc": "haml",
         "*.jst.eco": "html",
-        "*.blade.php": "html",
+        "*.blade.php": "blade",
         "*.tpl": "html",
         "*.jst": "html",
         "*.latte": "latte"
+    },
+
+    "files.exclude": {
+        "node_modules/": true
     },
 
     "editor.parameterHints": false,
@@ -20,12 +24,13 @@
     "editor.codeLens": false,
     "editor.renderLineHighlight": "none",
     "explorer.openEditors.visible": 0,
-    "workbench.colorTheme": "Default High Contrast",
+    "workbench.colorTheme": "Default Light+",
     "workbench.iconTheme": "vs-minimal",
-    "workbench.welcome.enabled": false,
     "editor.minimap.enabled": false,
     "emmet.triggerExpansionOnTab": true,
     "window.zoomLevel": -1,
     "editor.cursorStyle": "line",
-    "terminal.integrated.shell.windows": "C:\\Program Files\\Git\\bin\\bash.exe"
+    "liveServer.settings.donotShowInfoMsg": true,
+    "javascript.updateImportsOnFileMove.enabled": "never",
+    "editor.foldingStrategy": "indentation"
 }
